2e2c40ea6b5e3c2433bd7626c858fb8ff15d5685
Rob Wilson <rob.wilson@supergroup.co.uk>
2019-05-25 21:46:22 +0100

    Updated

--- src/BaseOptions.php
+++ src/BaseOptions.php
@@ -9,71 +9,71 @@ class BaseOptions
     {
         if ($this->yaml_loaded()) {
             return yaml_parse_file(__DIR__ . '/Options.yml');
-        } else {
+        }
 
-            return [
-                'Detection' => [
-                    'Rules' => [
-                        'Bots' => [
-                            'active' => 1,
-                            'score' => 1,
-                            'params' => 'sensu'
-                        ],
-                        'Html' => [
-                            'active' => 1,
-                            'score' => 1,
-                            'params' => '',
-                        ],
-                        'Numeric' => [
-                            'active' => 1,
-                            'score' => 1,
-                            'params' => '',
-                        ],
-                        'Url' => [
-                            'active' => 1,
-                            'score' => 1,
-                            'params' => '',
-                        ],
-                        'DisposableEmail' => [
-                            'active' => 1,
-                            'score' => 10,
-                            'params' => '',
-                        ],
-                        'InvalidEmail' => [
-                            'active' => 1,
-                            'score' => 1,
-                            'params' => '',
-                        ],
-                        'QueryStringKey' => [
-                            'active' => 1,
-                            'score' => 1,
-                            'params' => '',
-                        ],
-                        'QueryStringValue' => [
-                            'active' => 1,
-                            'score' => 1,
-                            'params' => '',
-                        ],
-                        'DifferentCountry' => [
-                            'active' => 1,
-                            'score' => 10,
-                            'params' => '',
-                        ],
-                        'Country' => [
-                            'active' => 1,
-                            'score' => 10,
-                            'params' => '',
-                        ],
-                        'Xss' => [
-                            'active' => 1,
-                            'score' => 10,
-                            'params' => '',
-                        ]
+        return [
+            'Detection' => [
+                'Rules' => [
+                    'Bots' => [
+                        'active' => 1,
+                        'score' => 1,
+                        'params' => 'sensu'
+                    ],
+                    'Html' => [
+                        'active' => 1,
+                        'score' => 1,
+                        'params' => '',
+                    ],
+                    'Numeric' => [
+                        'active' => 1,
+                        'score' => 1,
+                        'params' => '',
+                    ],
+                    'Url' => [
+                        'active' => 1,
+                        'score' => 1,
+                        'params' => '',
+                    ],
+                    'DisposableEmail' => [
+                        'active' => 1,
+                        'score' => 10,
+                        'params' => '',
+                    ],
+                    'InvalidEmail' => [
+                        'active' => 1,
+                        'score' => 1,
+                        'params' => '',
+                    ],
+                    'QueryStringKey' => [
+                        'active' => 1,
+                        'score' => 1,
+                        'params' => '',
+                    ],
+                    'QueryStringValue' => [
+                        'active' => 1,
+                        'score' => 1,
+                        'params' => '',
+                    ],
+                    'DifferentCountry' => [
+                        'active' => 1,
+                        'score' => 10,
+                        'params' => '',
+                    ],
+                    'Country' => [
+                        'active' => 1,
+                        'score' => 10,
+                        'params' => '',
+                    ],
+                    'Xss' => [
+                        'active' => 1,
+                        'score' => 10,
+                        'params' => '',
                     ]
                 ]
+            ]
 
-            ];
-        }
+        ];
+
     }
 
     private function yaml_loaded()
